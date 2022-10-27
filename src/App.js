224e0f69b189bfbd5058f53f27069e8f35778850
Wandberg de Lima <wandberg@gmail.com>
2022-10-26 23:16:31 -0300

    projeto de Postits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-
-function App() {
-  return (
-    <h1> Meu App React </h1>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
