9147c055f7a21954ce91e6117c1fee47de933bf1
Wandberg de Lima <wandberg@gmail.com>
2022-10-27 17:18:43 -0300

    Alterando a lista de Notas para Deleção.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
-
 class App extends Component {
 
   constructor(){
@@ -23,14 +22,23 @@ class App extends Component {
     this.setState(novoEstado)
   }
 
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas:arrayNotas})
+  }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas 
+        apagarNota={this.deletarNota.bind(this)}
+        notas={this.state.notas}/>
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
